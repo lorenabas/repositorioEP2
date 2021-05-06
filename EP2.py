4291891856f371b94ec9371a195c121598804980
Lorena Barbosa <lorenabas@al.insper.edu.br>
2021-05-06 20:56:20 -0300

    quarto commit

--- EP2.py
+++ EP2.py
@@ -14,3 +14,25 @@ def extrai_valor(carta):
     else:
         return carta[0]
 
+def lista_movimentos_possiveis(lista_baralho, indice):
+    movimentos_possiveis = []
+    if indice != 0:
+        carta_naipe = extrai_naipe(lista_baralho[indice])
+        carta_valor = extrai_valor(lista_baralho[indice])
+        if indice < 3:
+            anterior_naipe = extrai_naipe(lista_baralho[indice-1])
+            anterior_valor = extrai_valor(lista_baralho[indice-1])
+            if carta_naipe == anterior_naipe or carta_valor == anterior_valor:
+                movimentos_possiveis.append(1)
+        
+        else:
+            anterior_naipe = extrai_naipe(lista_baralho[indice-1])
+            anterior_valor = extrai_valor(lista_baralho[indice-1])
+            terceriro_anterior_naipe = extrai_naipe(lista_baralho[indice-3])
+            terceriro_anterior_valor = extrai_valor(lista_baralho[indice-3])
+            if carta_naipe == anterior_naipe or carta_valor == anterior_valor:
+                movimentos_possiveis.append(1)
+            if carta_naipe == terceriro_anterior_naipe or carta_valor == terceriro_anterior_valor:
+                movimentos_possiveis.append(3)
+
+    return movimentos_possiveis
