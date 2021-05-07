b52eb84e92de42e44deedc6b556384e9b12f7609
Lorena Barbosa <lorenabas@al.insper.edu.br>
2021-05-06 21:37:43 -0300

    oitavo commit

--- EP2.py
+++ EP2.py
@@ -19,24 +19,30 @@ def lista_movimentos_possiveis(lista_baralho, indice):
     if indice != 0:
         carta_naipe = extrai_naipe(lista_baralho[indice])
         carta_valor = extrai_valor(lista_baralho[indice])
+        anterior_naipe = extrai_naipe(lista_baralho[indice-1])
+        anterior_valor = extrai_valor(lista_baralho[indice-1])
         if indice < 3:
-            anterior_naipe = extrai_naipe(lista_baralho[indice-1])
-            anterior_valor = extrai_valor(lista_baralho[indice-1])
             if carta_naipe == anterior_naipe or carta_valor == anterior_valor:
                 movimentos_possiveis.append(1)
         
         else:
-            anterior_naipe = extrai_naipe(lista_baralho[indice-1])
-            anterior_valor = extrai_valor(lista_baralho[indice-1])
-            terceriro_anterior_naipe = extrai_naipe(lista_baralho[indice-3])
-            terceriro_anterior_valor = extrai_valor(lista_baralho[indice-3])
+            terceiro_anterior_naipe = extrai_naipe(lista_baralho[indice-3])
+            terceiro_anterior_valor = extrai_valor(lista_baralho[indice-3])
             if carta_naipe == anterior_naipe or carta_valor == anterior_valor:
                 movimentos_possiveis.append(1)
-            if carta_naipe == terceriro_anterior_naipe or carta_valor == terceriro_anterior_valor:
+            if carta_naipe == terceiro_anterior_naipe or carta_valor == terceiro_anterior_valor:
                 movimentos_possiveis.append(3)
 
     return movimentos_possiveis
 
+def possui_movimentos_possiveis(l_baralho):
+    v_ou_f = False
+    for i in range(len(l_baralho)):
+        if lista_movimentos_possiveis(l_baralho, i) != []:
+            v_ou_f = True
+    
+    return v_ou_f
+    
 def empilha(l_baralho, p_inicial, p_final):
     valor_inicial = l_baralho[p_inicial]
     valor_no_destino = l_baralho[p_final]
@@ -46,10 +52,3 @@ def empilha(l_baralho, p_inicial, p_final):
 
     return l_baralho
 
-def possui_movimentos_possiveis(l_baralho):
-    v_ou_f = False
-    for i in range(len(l_baralho)):
-        if lista_movimentos_possiveis(l_baralho, i) != []:
-            v_ou_f = True
-    
-    return v_ou_f
